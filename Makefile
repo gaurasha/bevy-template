15a53cbbc1f224ca17d7cd01a9af7b63711fc812
gauravsharma <gaurav.sharma@devrev.ai>
2023-02-12 13:54:11 +0530

    make deploy fix

--- Makefile
+++ Makefile
@@ -1,18 +1,25 @@
-.PHONY: run wasm web serve
+.PHONY: wasm dist-js deploy-js run serve 
+
+DIST_JS := dist/js
+wasm: 
+	cargo build --target wasm32-unknown-unknown
+
+dist-js: wasm
+	-rm -r ${DIST_JS}
+	mkdir ${DIST_JS}
+	cp target/wasm32-unknown-unknown/debug/bevy_github_ci_template.wasm ${DIST_JS}/bevy_game_bg.wasm
+	cp -r assets ${DIST_JS}/assets
+	cp wasm/* ${DIST_JS}
+	wasm-bindgen --no-typescript --out-name bevy_game --out-dir ${DIST_JS} --target web target/wasm32-unknown-unknown/debug/bevy_github_ci_template.wasm
+
+deploy-js: dist-js
+	-rm -rf tmp
+	git clone https://github.com/inqizit-public/bevy-template.git tmp
+	cd tmp; git checkout gh-pages; rm -rf *; cp -r ../dist/js/* .; git add .; git commit -m "deploy"; git push
+	-rm -rf tmp
 
 run: 
 	cargo run 
 
-wasm: 
-	cargo build --target wasm32-unknown-unknown
-
-web: wasm
-	rm -r dist
-	mkdir dist
-	cp target/wasm32-unknown-unknown/debug/bevy_github_ci_template.wasm dist/bevy_game_bg.wasm
-	cp -r assets dist/assets
-	cp wasm/* dist
-	wasm-bindgen --no-typescript --out-name bevy_game --out-dir dist --target web target/wasm32-unknown-unknown/debug/bevy_github_ci_template.wasm
-
-serve : web
-	python -m http.server 8000 --dir "dist"
+serve : dist-js
+	python -m http.server 8000 --dir "${DIST_JS}"
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,20 @@ fn main() {
             medium: Vec2::new(800.0, 600.0),
             small: Vec2::new(640.0, 360.0),
         })
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                fit_canvas_to_parent: true,
+                title: "Game of Life".to_string(),
+                // canvas: Some("#bevy".to_string()),
+                ..default()
+            },
+            ..default()
+        }))
         .add_startup_system(setup_camera)
         .add_startup_system(setup_ui)
         .add_system(on_resize_system)
         .add_system(toggle_resolution)
+        // .add_system(update_marker.after(on_resize_system))
         .run();
 }
 
@@ -41,6 +50,7 @@ fn setup_ui(mut cmd: Commands, asset_server: Res<AssetServer>) {
             size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
             ..default()
         },
+        background_color: BackgroundColor(Color::YELLOW),
         ..default()
     })
     .with_children(|root| {
@@ -55,8 +65,21 @@ fn setup_ui(mut cmd: Commands, asset_server: Res<AssetServer>) {
                 },
             ),
             ResolutionText,
+            // Transform::IDENTITY,
         ));
     });
+    // .with_children(|root| {
+    //     // Text where we display current resolution
+    //     root.spawn((
+    //         SpriteBundle {
+    //             texture: asset_server.load("icon.png"),
+    //             transform: Transform::from_xyz(100.0, 100.0, 100.0),
+    //             ..default()
+    //         },
+    //         Marker,
+    //         // Transform::IDENTITY,
+    //     ));
+    // });
 }
 
 /// This system shows how to request the window to a new resolution
@@ -91,5 +114,22 @@ fn on_resize_system(
     for e in resize_reader.iter() {
         // When resolution is being changed
         text.sections[0].value = format!("{:.1} x {:.1}", e.width, e.height);
+        // text.sections[0].
     }
 }
+
+// //Component to tag a marker
+// #[derive(Component)]
+// struct Marker;
+// //system that updates the position of the marker
+// fn update_marker(mut query: Query<&mut Transform, With<Marker>>, mut windows: ResMut<Windows>) {
+//     //window resource has changed, so lets update the position
+//     let window = windows.primary_mut();
+//     for mut transform in &mut query {
+//         transform.translation = find_bottom_right(&window);
+//     }
+// }
+// //help function that finds the bottom right coordinates of the window
+// fn find_bottom_right(window: &Window) -> Vec3 {
+//     Vec3::new(window.width() / 2.0, window.height() / -2.0, 0.0)
+// }
